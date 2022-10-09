886104fca4e8dc1fdd62df2ca6168f147a63f7b7
Elouan Lerissel <elouan.lerissel@gmail.com>
2022-10-09 13:51:18 +0200

    Working prototype with GraphQL github API

--- githubApi.js
+++ githubApi.js
@@ -1,113 +1,153 @@
-function requestUserRepos(username, nbrRepo){
-    const xhr = new XMLHttpRequest();
-    const url = `https://api.github.com/users/${username}/repos`;
-    xhr.open('GET', url, true);
-    xhr.onload = function () {
-        const data = JSON.parse(this.response);
-        const ul = document.getElementsByClassName('lang')[nbrRepo];
-        console.log(data);
+import {GithubAPI} from './private_token.js';
 
-        var urlLang = `https://api.github.com/repos/${data[nbrRepo].owner.login}/${data[nbrRepo].name}/languages`;
-        var xhrLang = new XMLHttpRequest();
-        var small = false;
-        xhrLang.open('GET', urlLang, true);
-        xhrLang.onload = function () {
-            const dataLang = JSON.parse(this.response);
-            const percent = {}
-            for(var i in dataLang){
-                percent[i] = (dataLang[i]/Object.values(dataLang).reduce((a,b)=>a+b))*100;
-                
+async function fetchData(obj){
+    return fetch('https://api.github.com/graphql', {
+        method: 'POST',
+        headers: {
+            'Authorization': ('bearer '+GithubAPI),
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            query: `
+                query 
+                {
+                viewer {
+                    login
+                    name
+                    #Repositories created by the viewer
+                    repositories(first:100,privacy:PUBLIC,ownerAffiliations:OWNER) {
+                        totalDiskUsage
+                        edges{
+                            node{
+                                name
+                                url
+                                owner {
+                                    login
+                                    avatarUrl
+                                }
+                                description
+                                updatedAt
+                                languages(first:100){
+                                    totalCount
+                                    totalSize
+                                    edges{
+                                        size
+                                        node{
+                                            name
+                                            color
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    #Repositories contributed to by the viewer
+                    repositoriesContributedTo(
+                    first: 100, 
+                    contributionTypes: [COMMIT, PULL_REQUEST]) 
+                    {
+                    totalCount,
+                    nodes {
+                        name
+                        owner {
+                            login
+                            avatarUrl
+                        }
+                        url,
+                        description,
+                        updatedAt
+                        languages(first: 100){
+                            totalCount,
+                            totalSize,
+                            edges{
+                                size,
+                                node {
+                                    name
+                                    color
+                                }
+                            }
+                        }
+                    }
+                    pageInfo {
+                        endCursor
+                        hasNextPage
+                    }
+                    }
+                }
             }
-            for(var i in percent){
-                if(percent[i] < 5){
-                    percent[i] = 5;      
-                    small = true
-                }
-                const li = document.createElement('li');
-                if(i == "HTML"){
-                    li.style.backgroundColor = "#e34c26";
-                }
-                if(i == "CSS"){
-                    li.style.backgroundColor = "#563d7c";
-                }
-                if(i == "JavaScript"){
-                    li.style.backgroundColor = "#f1e05a";
-                }
-                if(i == "PHP"){
-                    li.style.backgroundColor = "#4F5D95";
-                }
-                if(i == "C#"){
-                    li.style.backgroundColor = "#178600";
-                }
-                if(i == "C++"){
-                    li.style.backgroundColor = "#f34b7d";
-                }
-                if(i == "C"){
-                    li.style.backgroundColor = "#555555";
-                }
-                if(i == "Python"){
-                    li.style.backgroundColor = "#3572A5";
-                }
-                if(i == "Java"){
-                    li.style.backgroundColor = "#b07219";
-                }
-                if(i == "Shell"){
-                    li.style.backgroundColor = "#89e051";
-                }
-                if(i == "TypeScript"){
-                    li.style.backgroundColor = "#2b7489";
-                }
-                if(i == "Ruby"){
-                    li.style.backgroundColor = "#701516";
-                }
-                if(i == "Go"){
-                    li.style.backgroundColor = "#00ADD8";
-                }
-                if(i == "Rust"){
-                    li.style.backgroundColor = "#dea584";
-                }
-                if(i == "Swift"){
-                    li.style.backgroundColor = "#ffac45";
-                }
-                if(i == "Makefile"){
-                    li.style.backgroundColor = "#427819";
-                }
+            `
+        }) 
+    })
+    .then(res => res.json())
+    .then(data => {return data;})
+}
 
-                li.style.height = `${percent[i]}%`;
-                if(small){
-                    li.setAttribute('data-before', `${i} <5%`);
-                }else{
-                    li.setAttribute('data-before', `${i} ${percent[i].toFixed(1)}%`);
-                }
-                ul.appendChild(li);
-            }
-        }
-        xhrLang.send();
+async function fillTheList(data){
+    var response = await fetchData(); 
+    console.log(response);
+
+    console.log("response.data.viewer.repositories.edges : ", response.data.viewer.repositories.edges);
+    console.log("response.data.viewer.repositoriesContributedTo.nodes : ", response.data.viewer.repositoriesContributedTo.nodes);
 
 
-        let Name = document.getElementsByClassName("username")[nbrRepo];
-        let RepoName = document.getElementsByClassName("repository")[nbrRepo];
-        let RepoDesc = document.getElementsByClassName("p")[nbrRepo];
-        let Pdp = document.getElementsByClassName("pic")[nbrRepo];
-        let repoUrl = document.getElementsByClassName("repoUrl")[nbrRepo];
-        let topics = document.getElementsByClassName("topics")[nbrRepo];
-
-
-        Name.innerHTML = data[nbrRepo].owner.login;
-        RepoDesc.innerHTML = data[nbrRepo].description;
-        RepoName.innerHTML = "/"+data[nbrRepo].name;
-        Pdp.src = data[nbrRepo].owner.avatar_url;
-        repoUrl.href = data[nbrRepo].html_url;
-
-        for(var i in data[nbrRepo].topics){
-            const span = document.createElement('span');
-            span.innerHTML = data[nbrRepo].topics[i];
-            topics.appendChild(span);
-        }
+    //fill and sort the owned repos
+    for(var i in response.data.viewer.repositories.edges){
+        data.owner.push(response.data.viewer.repositories.edges[i].node);
     }
-    xhr.send();
+    data.owner.sort(function(a,b){
+        return new Date(b.updatedAt) - new Date(a.updatedAt);
+    })
+
+    //Fill and sort the other repos
+    for (var i in response.data.viewer.repositoriesContributedTo.nodes){
+        data.other.push(response.data.viewer.repositoriesContributedTo.nodes[i]);
+    }
+    data.other.sort(function(a,b){
+        return new Date(b.updatedAt) - new Date(a.updatedAt);
+    })
 }
 
-for(var i = 0; i < 3; i++){
-    requestUserRepos("LouvAndTech", i);
+
+async function fillUserRepo(repo,nbrRepo){
+
+    let Name = document.getElementsByClassName("username")[nbrRepo];
+    let RepoName = document.getElementsByClassName("repository")[nbrRepo];
+    let RepoDesc = document.getElementsByClassName("p")[nbrRepo];
+    let Pdp = document.getElementsByClassName("pic")[nbrRepo];
+    let repoUrl = document.getElementsByClassName("repoUrl")[nbrRepo];
+    let topics = document.getElementsByClassName("topics")[nbrRepo];
+
+
+    Name.innerHTML = repo.owner.login;
+    RepoDesc.innerHTML = repo.description;
+    RepoName.innerHTML = "/"+repo.name;
+    Pdp.src = repo.owner.avatarUrl;
+    repoUrl.href = repo.url;
+
+    for(var i in data[nbrRepo].topics){
+        const span = document.createElement('span');
+        span.innerHTML = data[nbrRepo].topics[i];
+        topics.appendChild(span);
+    }
 }
+
+
+/* Start */ 
+async function start(){
+    //fetch the Data and fill the list
+    var data = {
+        owner : [],
+        other : []
+    };
+    await fillTheList(data);
+    
+    //create html with the data
+    for(var i = 0; i < 3; i++){
+        fillUserRepo(data.owner[i], i);
+        fillUserRepo(data.other[i], i+3);
+    }
+    
+}
+
+start();
